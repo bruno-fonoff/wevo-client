318f3700eadee75bcfdd56ade29fdbbcd66560f1
bruno-fonoff <105615381+bruno-fonoff@users.noreply.github.com>
2022-08-28 13:38:50 -0300

    created navbar and search

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,12 +3,14 @@ import banner from "../../assets/images/company_logo.jpg";
 import "./style.css";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import user1 from "../../assets/images/user1.png";
-import user2 from "../../assets/images/user2.png";
+import user1 from "../../assets/images/user1.jpg";
+import user2 from "../../assets/images/user2.jpg";
 import { api } from "../../api/api";
+import { NavBar } from "../../components/NavBar";
 
 export function Home() {
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     async function allUsers() {
@@ -21,45 +23,52 @@ export function Home() {
 
   return (
     <>
+      <NavBar search={search} setSearch={setSearch} />
+
+      <img id="banner" src={banner} alt="banner wevo" />
       <div id="bodyHome">
         <div id="tituloCreateHome"></div>
-        <div>
-          <Link id="createPlay" to="/create" className="btn btn-primary"></Link>
-        </div>
-        <div id="home">
-          {users.map((currentUser) => {
-            return (
-              <>
-                <div id="cardsHome">
-                  <Link id="linkCard" to={`/user/${currentUser._id}`}>
-                    <div>
-                      <img
-                        id="fotoperfil"
-                        src={currentUser.sexo === "Masculino" ? user1 : user2}
-                        alt="foto-perfil"
-                      />
 
+        <div id="home">
+          {users
+            .filter((currentUser) => {
+              return currentUser.nome
+                .toLowerCase()
+                .includes(search.toLowerCase());
+            })
+            .map((currentUser) => {
+              return (
+                <>
+                  <div id="cardsHome">
+                    <Link id="linkCard" to={`/user/${currentUser._id}`}>
                       <div>
-                        <div className="card-body-home">
-                          <h4 className="card-title-home">
-                            {currentUser.nome}
-                          </h4>
-                          <hr />
-                          <p className="card-text-home">
-                            {currentUser.creation
-                              .slice(0, 10)
-                              .split("-")
-                              .reverse()
-                              .join("-")}
-                          </p>
+                        <img
+                          id="fotoperfil"
+                          src={currentUser.sexo === "Masculino" ? user1 : user2}
+                          alt="foto-perfil"
+                        />
+
+                        <div>
+                          <div className="card-body-home">
+                            <h4 className="card-title-home">
+                              {currentUser.nome}
+                            </h4>
+                            <hr />
+                            <p className="card-text-home">
+                              {currentUser.creation
+                                .slice(0, 10)
+                                .split("-")
+                                .reverse()
+                                .join("-")}
+                            </p>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </Link>
-                </div>
-              </>
-            );
-          })}
+                    </Link>
+                  </div>
+                </>
+              );
+            })}
         </div>
       </div>
     </>
